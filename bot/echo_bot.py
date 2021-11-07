c354282237c12bb0f4f2321efd7548b2633cf556
Margarie <mylipstastelike@gmail.com>
2021-11-08 00:42:48 +0300

    POST and GET telegram-bot
    - На любое сообщение создает новость с заголовком (текст сообщения)
    - Выводит новости на /show

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 
 bot = telebot.TeleBot("2066709794:AAELn19fE-WExz8LOh9xrz9yeWf5xzR3bKk", parse_mode=None)
 
@@ -9,5 +10,19 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: m.text == "test")
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['show'])
+def get_news_titles(message):
+    response = requests.get('http://127.0.0.1:8000/news/').json()
+    reply = ''
+    for news_item in response:
+        reply += f'id: {news_item["id"]}, title: {news_item["title"]}\n'
+    bot.reply_to(message, reply)
+
+@bot.message_handler(func=lambda m: True)
+def post_news_title(message):
+    response = requests.post('http://127.0.0.1:8000/news/', json={'title': message.text, 'category': '', 'text': ''})
+    if response.status_code == 200:
+        bot.reply_to(message, message.text)
 
 bot.infinity_polling()
