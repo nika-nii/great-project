60fdf70de9068144c1d72eb7a7ccf36789d52ccf
Margarie <mylipstastelike@gmail.com>
2022-01-06 00:46:06 +0300

    Очень плохо работающий код
    Тут добавлен валидатор и есть исправления в Post, но оно не работает
    При этом Get работает нормально
    Я долго колдовала, но не починила(((

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 from pymongo import MongoClient
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from bson import ObjectId
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,21 +27,46 @@ last_id = 0
 async def root():
     return {"message": "Приветики"}
 
+class PyObjectId(ObjectId):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
 class Post(BaseModel):
-    id: Optional[ObjectId()] #мб str или ничем
+    id: Optional[PyObjectId] = Field(alias="_id") #мб str или ничем
     title: str
     category: str
     text: str
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
-@app.post("/news/", response_model=Post)
+@app.post("/news/")
 async def create_post(post_in: Post):
     with MongoClient() as client:
         collection = client[DB][NEWS_COLLECTION]
-        result = collection.insert_one(post_in.dict())
+        if (hasattr(post_in, "id")):
+            delattr(post_in, "id")
+        result = collection.insert_one(post_in.dict(by_alias=True))
         res_id = result.inserted_id
         res_obj = collection.find_one(res_id)
         print(res_obj)
-        return Post(id = str(res_obj.id), title = res_obj.title, category = res_obj.title, text = res_obj.text)
+        post_in.id = res_id
+        return {"post_in": post_in}
+        # return Post(id = str(res_obj.id), title = res_obj.title, category = res_obj.title, text = res_obj.text)
 
 @app.get("/news/", response_model=List[Post])
 async def show_posts():
